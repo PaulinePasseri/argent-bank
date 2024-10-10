4ad68d2411d4e3109f7caa4e35d322240f5b1181
Pauline Passeri <paulinecpasseri@gmail.com>
2024-10-10 15:53:06 +0200

    correction du header selon le path

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,8 +1,14 @@
 import HomeLogo from '../../assets/argentBankLogo.png'
 import './header.css'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
-export default function Header() {
+export default function Header({name}) {
+    const location = useLocation()
+    const [activeLink, setActiveLink] = useState(location.pathname)
+    useEffect(() => {
+      setActiveLink(location.pathname)
+    }, [location.pathname])
     return (
         <nav className="main-nav">
           <Link className="main-nav-logo" to="/">
@@ -13,12 +19,28 @@ export default function Header() {
             />
             <h1 className="sr-only">Argent Bank</h1>
           </Link>
+
+          
           <div>
-            <Link className="main-nav-item" to="/signin">
-              <i className="fa fa-user-circle"></i>
-              Sign In
-            </Link>
+            {activeLink === '/user' ? (
+              <>
+                <Link className="main-nav-item" to="/user">
+                  <i className="fa fa-user-circle"></i>
+                  {name}
+                </Link>
+                <Link className="main-nav-item" to="/">
+                  <i className="fa fa-sign-out"></i>
+                  Sign Out
+                </Link>
+              </>
+            ) : (
+              <Link className="main-nav-item" to="/signin">
+                <i className="fa fa-user-circle"></i>
+                Sign In
+              </Link>
+            )}
           </div>
         </nav>
     )
 }
+ 
