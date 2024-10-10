1c7708da19588066367f505c15362f8d71e605bd
Pauline Passeri <paulinecpasseri@gmail.com>
2024-10-10 12:00:50 +0200

    header et footer finalisés

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,11 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header/header'
+import Footer from './components/Footer/footer'
 import Home from './pages/home'
 import User from './pages/user'
 import SignIn from './pages/sign-in'
+import './main.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -18,6 +20,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route path="/user" element={<User />} />
         </Routes>
       </div>
+      <Footer />
     </Router>
   </React.StrictMode>,
 )
